60526a3ba5e01ea2b8ca287369b0f476252db3e8
AngelIpina12 <angelipina12@hotmail.com>
2025-01-27 12:26:07 -0600

    Add custom button component

---
+++ src/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Button = ({
+    children,
+    primary,
+    secondary,
+    success,
+    warning,
+    danger,
+    outline,
+    rounded,
+}) => {
+    
+  return (
+    <button>{children}</button>
+  )
+}
