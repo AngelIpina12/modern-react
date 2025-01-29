a25e8d043a7b053fdee10facfd10749c6e67f3d9
AngelIpina12 <angelipina12@hotmail.com>
2025-01-29 14:58:44 -0600

    Add modal structure in Modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const Modal = () => {
   return (
-    <div>Modal</div>
+    <div>
+      <div className='absolute inset-0 bg-gray-300 opacity-80'></div>
+      <div className='absolute inset-40 p-10 bg-white'>
+        It's a modal
+      </div>
+    </div>
   )
 }
--- src/pages/ModalPage.jsx
+++ src/pages/ModalPage.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Modal } from '../components/Modal'
+import { Button } from '../components/Button'
 
 export const ModalPage = () => {
+    const [showModal, setShowModal] = useState(false);
+    const handleClick = () => {
+        setShowModal(true);
+    }
+
     return (
         <div>
-            <Modal />
+            <Button primary onClick={handleClick}>Open Modal</Button>
+            {showModal && <Modal />}
         </div>
     )
 }
