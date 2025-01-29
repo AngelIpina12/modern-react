f72879faae7ac6118e0304c4091ce8d02180a788
AngelIpina12 <angelipina12@hotmail.com>
2025-01-29 10:39:30 -0600

    Add DropdownPage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,4 @@
-import { useState } from "react"
-import { Dropdown } from "./components/Dropdown"
 
 export const App = () => {
-    const [selection, setSelection] = useState(null);
-    const handleSelect = (option) => {
-        setSelection(option);
-    }
-    const options = [
-        { label: "Red", value: "red" },
-        { label: "Green", value: "green" },
-        { label: "Blue", value: "blue" },
-    ]
-
-    return <Dropdown options={options} value={selection} onChange={handleSelect} />
+    return <div></div>
 }
