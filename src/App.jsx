0eac48a91f962731b274f9d4f91e746c8f83dc2a
AngelIpina12 <angelipina12@hotmail.com>
2025-01-29 11:25:07 -0600

    Add Link component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import { Link } from "./components/Link"
 
 export const App = () => {
-    return <div></div>
+    return <div>
+        <Link to="/accordion">Go to accordion</Link>
+        <Link to="/dropdown">Go to dropdown</Link>
+    </div>
 }
